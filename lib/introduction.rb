259e5bdf060f0188ca9c79773d604792e38448aa
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-21 02:29:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,7 +19,12 @@ end
 
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+  name = "Dan"
+  
+end 
+  
 
 
 
